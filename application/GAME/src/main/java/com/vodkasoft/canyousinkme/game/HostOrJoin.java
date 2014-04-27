7507071ccb7556cabd824319df0c45b728f07cc9
Albin Arias Juárez <alarias@ic-itcr.ac.cr>
2014-04-27 17:07:22 -0600

    Update GUI

--- application/GAME/src/main/java/com/vodkasoft/canyousinkme/game/HostOrJoin.java
+++ application/GAME/src/main/java/com/vodkasoft/canyousinkme/game/HostOrJoin.java
@@ -1,13 +1,13 @@
 package com.vodkasoft.canyousinkme.game;
 
+import com.vodkasoft.canyousinkme.connectivity.BleutoothManager;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
-import com.vodkasoft.canyousinkme.connectivity.BleutoothManager;
 
 public class HostOrJoin extends Activity {
 
